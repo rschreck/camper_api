6c50fa8eced3f016911dd26934deb885ae68f485
Renuka Schreck <schrer3@gmail.com>
2021-04-14 06:58:45 -0500

    app.js first route

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ dotenv.config({
 const apidocs = fs.readFileSync("./docs/apidocs.json");
 
 const PORT = process.env.PORT || 4002;
-
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true });
+});
 app.listen(PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
