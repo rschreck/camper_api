11f7ba3f402b2e6faa58ab2b9bbf816def99775d
Renuka Schreck <schrer3@gmail.com>
2021-04-18 17:16:08 -0500

    routes folder created for routes and unit test location changed

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const dotenv = require("dotenv");
 const fs = require("fs");
 //Route files
 const bootcamps = require("./routes/bootcamp");
+const root = require("./routes/root");
 ntlm = require("express-ntlm");
 dotenv.config({
   path: "./config/config.env",
 });
-
+app.use("/", root);
 app.use("/v1/bootcamps", bootcamps);
 const apidocs = fs.readFileSync("./docs/apidocs.json");
 
