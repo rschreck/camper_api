5f5f08ce8c324425f129b6c46ce3bd3622aba316
Renuka Schreck <schrer3@gmail.com>
2021-05-18 20:16:34 -0500

    middleware, readme for routes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# camper_api
+# camper_api routes
+
+/v1/bootcamps - GET ALL bootcamps
+/v1/bootcamps - POST Create New bootcamp
+
+/v1/bootcamp/60a2568329ace81dc027228b GET
+/v1/bootcamp/60a2568329ace81dc027228b PUT
+/v1/bootcamp/60a2568329ace81dc027228b DELETE
--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use(function (req, res, next) {
+  console.log(`url: ${req.url}, method:${req.method}`);
+  next();
+});
 router.get("/", function (req, res) {
   res
     .status(200)
